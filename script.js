40c9cbc50195a25c274d0c50c61725e0cde80413
jshokirov789-del <jshokirov789@gmail.com>
2026-02-26 11:54:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function chiqar(){
+    document.getElementById("outName").innerText =
+        document.getElementById("name").value;
+
+    document.getElementById("outEmail").innerText =
+        document.getElementById("email").value;
+
+    document.getElementById("outPassword").innerText =
+        document.getElementById("password").value;
+}
